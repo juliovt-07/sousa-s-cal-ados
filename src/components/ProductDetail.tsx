32ea438b43ca6f2c44549c1c49197a3b25ad71ca
Julio Cesar <juliocesaar.ma@gmail.com>
2025-05-15 14:15:29 -0300

    fix: remove useParams unused

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import useFetchData from '../hooks/useFetchData';
 import { useCart } from '../hooks/useCart';
